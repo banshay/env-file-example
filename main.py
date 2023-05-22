13236709ee1140b339c262228dabea2466e98b92
banshay <importantbopp@gmail.com>
2023-05-22 21:17:03 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from dotenv import load_dotenv
 import os
 
@@ -11,14 +7,8 @@ NOT_SECRET_API_KEY = "Sadge"
 
 API_KEY = os.getenv("API_KEY")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(API_KEY)
     print(NOT_SECRET_API_KEY)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
